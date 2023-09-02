5f0e7a9241ce4a6481f024e6949bfd89147435fd
18577447378 <dadg39@qq.com>
2023-09-02 12:34:34 +0800

    项目建立

---
+++ app/chat/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+export interface ChatPageProps {
+  params: {
+    id: string
+  }
+}
+
+export default function ChatPage() {
+
+  const isLogin = false
+
+
+  return (
+    <div>聊天页面</div>
+  )
+}
---
+++ app/sign-in/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SignInPage() {
+  return (
+    <div>登录页面</div>
+  )
+}
